4606f85cfac0e400f32fffabf224cf918819fb80
Avijeet pandey <avijeetpandey25@gmail.com>
2024-03-30 13:44:11 +0530

    Initial Commit

---
+++ toggle-example/toggle_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  toggle_exampleApp.swift
+//  toggle-example
+//
+//  Created by Avijeet on 30/03/24.
+//
+
+import SwiftUI
+
+@main
+struct toggle_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
